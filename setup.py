b1bf547cb816b96d720207b5c8c39b208af9a81d
tfederico <federicotavella.7@gmail.com>
2021-10-22 15:15:51 +0100

    update setup file with new package

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='ASL phonetic_classification',
       version='1.0',
       description='',
       author='Federico Tavella',
-      packages=['deep_learning', 'machine_learning', "data"],
+      packages=['deep_learning', 'machine_learning', "data", "utils"],
      )
