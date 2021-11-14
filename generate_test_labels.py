25ae7c43dfa96a2326b57c57b93a890057bf37e9
tfederico <federicotavella.7@gmail.com>
2021-11-14 13:42:23 +0000

    update script for labels generation

--- generate_test_labels.py
+++ generate_test_labels.py
@@ -1,42 +1,46 @@
 import pickle as pkl
 import json
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--label", required=True, type=str, choices=["MajorLocation", "SignType", "Movement"])
-parser.add_argument("--tracker", required=True, type=str, choices=["frank", "hrnet"])
-parser.add_argument("--zero_shot", action='store_true')
+labels = ["MajorLocation", "Movement", "SignType", "MinorLocation", "Flexion", "SelectedFingers"]
+trackers = ["frank", "hrnet"]
+zero_shots = [True, False]
 
-args = parser.parse_args()
-label = args.label.lower()
-tracker = args.tracker.replace("hrnet", "hrt")
-zero_shot = "-zs" if args.zero_shot else ""
-folder = "27-frank-frank" if tracker == "frank" else "27_2-hrt"
-folder += zero_shot
-tracker += zero_shot
 
-with open(f"data/npy/{label}/{folder}/test_label_{tracker}.pkl", "rb") as fp:
-    test_label = pkl.load(fp)
+for zero_shot in zero_shots:
+    print(zero_shot)
+    for label in labels:
+        print(label)
+        for tracker in trackers:
+            print(tracker)
+            label = label.lower()
+            tracker = tracker.replace("hrnet", "hrt")
+            suffix = "-zs" if zero_shot else ""
+            folder = "27-frank-frank" if tracker == "frank" else "27_2-hrt"
+            folder += suffix
+            tracker += suffix
+            print(folder, tracker, label)
+            with open(f"data/npy/{label}/{folder}/test_label_{tracker}.pkl", "rb") as fp:
+                test_label = pkl.load(fp)
 
-test_label = zip(test_label[0], test_label[1])
+            test_label = zip(test_label[0], test_label[1])
 
-with open(f"data/npy/{label}/{folder}/split.json", "r") as fp:
-    split = json.load(fp)
+            with open(f"data/npy/{label}/{folder}/split.json", "r") as fp:
+                split = json.load(fp)
 
-test_split = split["test"]
+            test_split = split["test"]
 
-with open(f"data/npy/{label}/{folder}//label2id.json", "r") as fp:
-    label2class_id = json.load(fp)
+            with open(f"data/npy/{label}/{folder}//label2id.json", "r") as fp:
+                label2class_id = json.load(fp)
 
-class_id2label = {v: k for k, v in label2class_id.items()}
+            class_id2label = {v: k for k, v in label2class_id.items()}
 
-with open(f"data/npy/{label}/{folder}/file2gloss.json", "r") as fp:
-    file2gloss = json.load(fp)
+            with open(f"data/npy/{label}/{folder}/file2gloss.json", "r") as fp:
+                file2gloss = json.load(fp)
 
-video_id2label = {}
-for video_id, class_id in test_label:
-    video_id2label[video_id] = class_id2label[class_id]
+            video_id2label = {}
+            for video_id, class_id in test_label:
+                video_id2label[video_id] = class_id2label[class_id]
 
-with open(f"data/npy/{label}/{folder}/gt.csv", "w") as fp:
-    for video_id, label in sorted(video_id2label.items()):
-        fp.write(','.join([video_id, file2gloss[video_id], label])+"\n")
+            with open(f"data/npy/{label}/{folder}/gt.csv", "w") as fp:
+                for video_id, lab in sorted(video_id2label.items()):
+                    fp.write(','.join([video_id, file2gloss[video_id], lab])+"\n")
