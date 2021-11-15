c85e90165f995da820b3e4c454ad64dd5e67ae50
Federico <tfederico@users.noreply.github.com>
2021-11-15 09:33:30 +0000

    fix missing ids_test when loading 3d cnn data

--- deep_learning/test.py
+++ deep_learning/test.py
@@ -123,6 +123,8 @@ def main(args):
                                                     window_size=args.window_size, tracker=args.tracker, set_type="test",
                                                     suffix=suffix, drop_features=drop_features, different_length=True,
                                                     transform=transforms)
+            ids_test = test_dataset.motions_keys
+            print(ids_test)
             with open("data/npy/{}/{}/label2id.json".format(sel_labels[0].lower(), args.tracker), "rb") as fp:
                 label2id = json.load(fp)
     else:
